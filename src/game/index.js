39ba846c43b42582da97f5cce408d0a5370eaa5d
Gilles <contact@gb-prod.fr>
2016-10-01 11:53:55 +0200

    bar !!!

--- src/game/index.js
+++ src/game/index.js
@@ -2,9 +2,11 @@ var Phaser = require('phaser');
 
 var SCALE = 3;
 var BALL_SPEED = 300;
+var BAR_SPEED = 100;
 
 var ball;
-
+var cursor;
+var bar;
 
 var game = new Phaser.Game(
   // Game width
@@ -30,13 +32,17 @@ var game = new Phaser.Game(
 function preload() {
   game.load.image('ball-image', 'game/assets/pokeball.png');
   game.load.image('background-image', 'game/assets/pokebackground.png');
+  game.load.image('bar-image', 'game/assets/bar.png');
 }
 
 function create() {
   game.stage.backgroundColor = "#4488AA";
   game.add.image(0, 0, 'background-image');
 
+  cursor = game.input.keyboard.createCursorKeys();
+
   ball = createBall();
+  bar = createBar();
 }
 
 function createBall() {
@@ -55,6 +61,22 @@ function createBall() {
   return ball;
 }
 
+function createBar() {
+  var bar = game.add.sprite(game.world.centerX - 48, 440, 'bar-image');
+  bar.scale.set(SCALE);
+  game.physics.enable(bar, Phaser.Physics.ARCADE);
+  bar.body.collideWorldBounds = true;
+
+
+  return bar;
+}
+
 function update() {
-  ball.angle += 4;
+  ball.angle += 8; // drunk mode
+  bar.body.velocity.x = 0;
+  if (cursor.left.isDown) {
+    bar.body.velocity.x = - BAR_SPEED * SCALE;
+  } else if (cursor.right.isDown) {
+    bar.body.velocity.x = BAR_SPEED * SCALE;
+  }
 }
