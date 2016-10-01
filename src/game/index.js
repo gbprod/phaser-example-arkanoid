3d0fceaf89f31f56edb34753d246ae553c292f2b
Gilles <contact@gb-prod.fr>
2016-10-01 11:12:18 +0200

    background and physics

--- src/game/index.js
+++ src/game/index.js
@@ -1,23 +1,25 @@
 var Phaser = require('phaser');
 
 var SCALE = 3;
+var BALL_SPEED = 400;
 
 var ball;
 
+
 var game = new Phaser.Game(
   // Game width
-  120 * SCALE,
+  800,
   // Game height
-  60 * SCALE,
+  480,
   // Game renderer (WebGL, Canvas, auto)
   Phaser.AUTO,
   // Game id in index.html
   'phaser-example-arkanoid',
   // Phaser states
   {
-    preload: _preload,
-    create: _create,
-    update: _update
+    preload: preload,
+    create: create,
+    update: update
   },
   // Transparent canvas background
   false,
@@ -25,21 +27,28 @@ var game = new Phaser.Game(
   false
 );
 
-function _preload() {
-  console.log('Preload game');
+function preload() {
   game.load.image('ball-image', 'game/assets/pokeball.png');
-
+  game.load.image('background-image', 'game/assets/pokebackground.png');
 }
 
-function _create() {
-  console.log('Create game');
+function create() {
   game.stage.backgroundColor = "#4488AA";
+  game.add.image(0, 0, 'background-image');
+
   ball = game.add.sprite(0, 50, 'ball-image');
+  game.physics.enable(ball, Phaser.Physics.ARCADE);
+
+  ball.body.collideWorldBounds = true;
+  ball.body.bounce.set(1);
+
+  var angle = 120 * 360 / Math.PI;
+  ball.body.velocity.setTo(
+    Math.cos(angle) * BALL_SPEED,
+    Math.sin(angle) * BALL_SPEED
+  );
 }
 
-function _update() {
-  ball.x += 1;
-  if (ball.x >= 120 * SCALE) {
-    ball.x = 1;
-  }
+function update() {
+
 }
